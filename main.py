c01460e3c7c20e56fbd7803122412e3ee21a8dd2
julien <1739151@cll.qc.ca>
2021-04-29 18:22:56 -0400

    added some stuff

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import gym
 
 env_name = "CartPole-v1"
-step = 100
+nb_step = 100
+nb_story = 10
 env = gym.make(env_name)
 env.seed(100)
 
 for i_episode in range(10):
     observation = env.reset()
 
-    for t in range(step):
+    for t in range(nb_step):
         env.render()
         action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
+        observation, reward, done, info = env.nb_step(action)
         time.sleep((1/60))
-        print("State: " + str(round(t * 100 / step, 2)) + "%", flush=True, end='\r')
+        print("State: " + str(round(t * 100 / nb_step, 2)) + "%", flush=True, end='\r')
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
+            print("Episode finished after {} timenb_steps".format(t+1))
             print("Fininsh at " + str(t))
             break
-        
         
 env.close()
