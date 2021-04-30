8c407d4a6fc3ef0102098829d459111c3b06e9ad
julien <1739151@cll.qc.ca>
2021-04-29 20:13:28 -0400

    commented some stuff

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import gym
 
+#--------------------------------------------------------------------------------------------------------------------------------
+#CARTPOLE reinforcement learning
+
+#Observation: (Cart position, Cart Velocity, Pole angle, Pole velocity at tip)
+#Action: right = 1, left = 0
+#Reward return 0  if pole angle more than ±12° or Cart Position is more than ±2.4  return 1 if condition are respected
+#Goal: having the highest reward count possible
+
 env_name = "CartPole-v1"
 nb_step = 100
 nb_story = 10
+reward_count = 0
 env = gym.make(env_name)
 env.seed(100)
 
-for i_episode in range(10):
+for i_episode in range(nb_story):
     observation = env.reset()
 
-    for t in range(nb_step):
+    for t in range(300):
         env.render()
-        action = env.action_space.sample()
-        observation, reward, done, info = env.nb_step(action)
-        time.sleep((1/60))
-        print("State: " + str(round(t * 100 / nb_step, 2)) + "%", flush=True, end='\r')
+        action = 1
+        observation, reward, done, info = env.step(action)
         if done:
-            print("Episode finished after {} timenb_steps".format(t+1))
-            print("Fininsh at " + str(t))
+            reward_count = t
             break
-        
+    
+    print(reward_count)
+
 env.close()
