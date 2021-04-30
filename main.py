59ecee35f3731370cb50c6e4dcc8daeccf54449e
julien <1739151@cll.qc.ca>
2021-04-30 13:38:27 -0400

    commented more stuff and added a graph

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import gym
+import random
+import time
+import matplotlib.pyplot as plt
 
 #--------------------------------------------------------------------------------------------------------------------------------
 #CARTPOLE reinforcement learning
@@ -11,21 +14,30 @@ import gym
 env_name = "CartPole-v1"
 nb_step = 100
 nb_story = 10
-reward_count = 0
+reward_count = []
 env = gym.make(env_name)
 env.seed(100)
 
-for i_episode in range(nb_story):
-    observation = env.reset()
+for i_episode in range(nb_story): 
+    observation = env.reset() # return initial value of env
 
-    for t in range(300):
-        env.render()
-        action = 1
-        observation, reward, done, info = env.step(action)
-        if done:
-            reward_count = t
+    for t in range(1000): #max number of step
+        env.render() #start the simulation
+        action = random.randint(0, 1) #Random action
+        observation, reward, done, info = env.step(action) # return values
+        
+        if done: # check if simulation has failed
+            reward_count.append(round(t/60, 2)) # had result into array
             break
+        time.sleep(1/60)
     
     print(reward_count)
 
 env.close()
+
+
+#Print result
+plt.plot(reward_count)
+plt.ylabel("Time balance")
+plt.show()
+
